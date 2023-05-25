91a1ce0d3d7b2783f39f3e3796a6b8272553a20c
gpalaciosg-at-243505175593 <guipalaga@gmail.com>
2023-05-24 18:32:11 -0600

    feat: Adding vpc.tf and vpc variables

--- variables.tf
+++ variables.tf
@@ -7,3 +7,38 @@ variable "region" {
   default     = "us-east-1"
   type        = string
 }
+
+variable "tags" {
+  description = "Tags to propogate to all supported resources"
+  type        = map(string)
+  default = {
+    "terraform_managed" = "true"
+  }
+}
+
+//----------------------------------------------------------------------
+// VPC  Variables
+//----------------------------------------------------------------------
+variable "vpc_name" {
+  description = "Name of the VPC to create"
+  type        = string
+  default     = "Primary VPC"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR associated with the VPC to be created"
+  type        = string
+  default     = "10.1.0.0/16"
+}
+
+variable "enable_dns_hostnames" {
+  description = "Should be true to enable DNS hostnames in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_support" {
+  description = "Should be true to enable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
