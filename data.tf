421bc28d1643370ebafe1bd06034dfcf5f3aff5f
gpalaciosg-at-243505175593 <guipalaga@gmail.com>
2023-05-24 20:30:14 -0600

    fix: Updating data.tf

--- data.tf
+++ data.tf
@@ -1,1 +1,32 @@
 data "aws_availability_zones" "available_az" {}
+
+data "aws_ami" "amazon_linux" {
+  
+  most_recent = true
+  name_regex  = "^amzn2*"
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "template_file" "webserver_launch_configuration_user_data" {
+  template = file("${path.module}/template/user_data.sh")
+}
