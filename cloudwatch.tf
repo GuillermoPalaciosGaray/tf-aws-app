39e3f76277c822604a0195af39f549caa9f9fe42
gpalaciosg-at-243505175593 <guipalaga@gmail.com>
2023-05-25 07:53:01 -0600

    fix: Fixing cloudwatch alarm typo

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
   alarm_name          = "${var.cw_alarm_name}cpu_up"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 2
-  metric_name         = "CPU_Utilization"
+  metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
   period              = 120
   statistic           = "Average"
@@ -21,7 +21,7 @@ resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_down" {
   alarm_name          = "${var.cw_alarm_name}cpu_down"
   comparison_operator = "LessThanOrEqualToThreshold"
   evaluation_periods  = 2
-  metric_name         = "CPU_Utilization"
+  metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
   period              = 120
   statistic           = "Average"
