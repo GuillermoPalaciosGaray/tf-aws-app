39887d04fe6433f2a787a00c19bac7d9dbcfcac7
gpalaciosg-at-243505175593 <guipalaga@gmail.com>
2023-05-25 11:13:33 -0600

    fix: Updating data.tf to fetch latest free tier AWS Linux AMI

--- data.tf
+++ data.tf
@@ -3,12 +3,12 @@ data "aws_availability_zones" "available_az" {}
 data "aws_ami" "amazon_linux" {
   
   most_recent = true
-  name_regex  = "^amzn2*"
+  name_regex  = "^al2023-ami-2023*"
   owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-gp2"]
+    values = ["al2023-ami*-x86_64"]
   }
 
   filter {
